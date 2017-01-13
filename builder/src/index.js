6c4c3d502bc906fe5bb150f37e0206b3aa208d39
Jesse Sutherland <jesse.a.sutherland@gmail.com>
2017-01-13 15:43:23 -0500

    added my own, slightly overridden version of blueprint css cause I dont like the huge one they ship, messes up some layouts, toasts work in a way that i htink is broken

--- builder/src/index.js
+++ builder/src/index.js
@@ -4,7 +4,9 @@ import App from './App';
 import './index.css';
 import 'fullcalendar/dist/fullcalendar.min.css';
 // Annoyingly I get a bunch of other css w/ blueprint, not just the toast stuff.
-import '@blueprintjs/core/dist/blueprint.css';
+// import '@blueprintjs/core/dist/blueprint.css';
+// So f, that, I'll have my own version.
+import './blueprint.css';
 
 ReactDOM.render(
   <App />,
