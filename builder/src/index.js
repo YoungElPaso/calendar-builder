1bc912105e258dda82dc1deebacb9d5600ec8b8c
Jesse Sutherland <jesse.a.sutherland@gmail.com>
2017-02-07 15:23:21 -0500

    added a more meaningful comment about blueprint css import

--- builder/src/index.js
+++ builder/src/index.js
@@ -3,10 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import 'fullcalendar/dist/fullcalendar.min.css';
-// Annoyingly I get a bunch of other css w/ blueprint, not just the toast stuff.
+// Import all of blueprint css - not great.
 import '@blueprintjs/core/dist/blueprint.css';
-// So f, that, I'll have my own version.
-// import './blueprint.css';
 
 ReactDOM.render(
   <App />,
