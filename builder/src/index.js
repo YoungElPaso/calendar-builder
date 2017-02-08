225fc820dd0e4aabbce70634d33e3de1243429bb
Jesse Sutherland <jesse.a.sutherland@gmail.com>
2017-02-08 00:43:30 -0500

    implemented super simple router, root for main app (builder) /client for client variant (no UI really)

--- builder/src/index.js
+++ builder/src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// Import react-router modules.
+import { Router, Route, Link, browserHistory} from 'react-router'
+// Import the main App component.
 import App from './App';
+// Import the Client component.
+import Client from './Client';
+
 import './index.css';
 import 'fullcalendar/dist/fullcalendar.min.css';
 // Import all of blueprint css - not great.
 import '@blueprintjs/core/dist/blueprint.css';
 
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory}>
+    <Route path="/" component={App} />
+    <Route path="/client" component={Client} />
+  </Router>,
   document.getElementById('root')
 );
